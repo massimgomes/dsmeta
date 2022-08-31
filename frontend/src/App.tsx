37a1b5f6b542074eb9af2eaacbd016ab61498f2e
massimgomes <massimgomes14@gmail.com>
2022-08-31 12:48:06 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components"
 
 function App() {
-   return(
-    <h1>Olá mundo</h1>
-   )
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
